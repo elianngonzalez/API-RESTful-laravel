fe33901f5e0f77183ba484de9bfc84085f763a9a
elian gonzalez <elianngonzalez9@gmail.com>
2022-04-17 17:32:30 -0300

    api response

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
-//use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,28 +31,36 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('products', function () {
-	return response(Product::all(), 200);
-});
+// Route::get('products', function () {
+// 	return response(Product::all(), 200);
+// });
 
-Route::get('products/{product}', function ($productId) {
-	return response(Product::find($productId), 200);
-});
+// Route::get('products/{product}', function ($productId) {
+// 	return response(Product::find($productId), 200);
+// });
 
 
-Route::post('products', function (Request $request) {
-	$resp = Product::create($request->all());
-	return $resp;
-});
+// Route::post('products', function (Request $request) {
+// 	$resp = Product::create($request->all());
+// 	return $resp;
+// });
 
-Route::put('products/{product}', function (Request $request, $productId) {
-	$product = Product::findOrFail($productId);
-	$product->update($request->all());
-	return $product;
-});
+// Route::put('products/{product}', function (Request $request, $productId) {
+// 	$product = Product::findOrFail($productId);
+// 	$product->update($request->all());
+// 	return $product;
+// });
 
-Route::delete('products/{product}', function ($productId) {
-	Product::find($productId)->delete();
+// Route::delete('products/{product}', function ($productId) {
+// 	Product::find($productId)->delete();
 
-	return 204;
+// 	return 204;
+// });
+
+Route::controller(ProductsController::class)->group(function () {
+	Route::get('products', 'index');
+	Route::get('products/{product}', 'show');
+	Route::post('products', 'store');
+	Route::put('products/{product}', 'update');
+	Route::delete('products/{product}', 'destroy');
 });
